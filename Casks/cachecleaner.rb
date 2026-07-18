3553bd063dbf99499d5e2de3e0293c37b67f4fe9
Alexey Nazarov <nazarov@ptr.vc>
2026-07-18 11:33:45 +0300

    cachecleaner 1.0.0: refresh dmg checksum

--- Casks/cachecleaner.rb
+++ Casks/cachecleaner.rb
@@ -3,7 +3,7 @@
 # After each release: bump `version`, paste sha256 printed by scripts/release.sh.
 cask "cachecleaner" do
   version "1.0.0"
-  sha256 "e66857a17125116ca8e616f98c1af4a7bf2adafd1f94301d9db44d268da498b6"
+  sha256 "5d4a5543b9e05cd3554b16558611cd6041c880f652bd171a673992ea1af8ff7b"
 
   url "https://cachecleaner.app/downloads/CacheCleaner-#{version}.dmg"
   name "CacheCleaner"
